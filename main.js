9f0037f14246379b2d4ae6c6ef107eb0dd0bf2df
Katie Miller <katie.miller@netlify.com>
2022-05-29 23:10:32 +0200

    Begin card refinement with file upload per cell

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 import './style.css'
+window.readURL = (input) => {
+  console.log("readURL %o", input);
+  if (input.files && input.files[0]) {
+    console.log(input.files[0]);
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      console.log(e.target.result);
+      localStorage.setItem("imgData", e.target.result);
+    }
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
